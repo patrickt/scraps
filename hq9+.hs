402d8dbdd75cf4b4b5cac561ea79aaa0e212735a
Patrick Thomson <ironswallow@gmail.com>
2008-11-03 13:21:46 -0500

    I had some errors. They're fixed now.

--- hq9+.hs
+++ hq9+.hs
@@ -5,42 +5,48 @@
 module Main where
   
   import System.Environment -- for command-line arguments
-  import Data.IORef         -- for IORefs
   import Text.Printf        -- for simplified printing
+  
+  pluralize :: Integer -> String
+  pluralize 1 = "bottle"
+  pluralize _ = "bottles"
   
   -- prints out 99 bottles of beer on the wall. recursive and functional.
   bottles :: Integer -> IO ()
-  
-  bottles 0 = putStrLn "NO MOAR BEER." -- a tragic situation
-  
+  bottles 0 = putStrLn "NO MOAR BEER."
   bottles n =
-    printf "%d bottles of beer on the wall,\n" n          >>
-    printf "%d bottles of beer,\n" n                      >>
-    printf "Take one down, pass it around,\n"             >>
-    printf "%d bottles of beer on the wall\n\n" (n - 1)   >>
-    bottles (n - 1)
+    let b = (pluralize n) in
+      printf "%d %s of beer on the wall,\n" n b          >>
+      printf "%d %s of beer,\n" n b                      >>
+      printf "Take one down, pass it around,\n"          >>
+      printf "%d %s of beer on the wall\n\n" (n - 1) b   >>
+      bottles (n - 1)
   
   -- Prints out all passed arguments.
   printquine :: IO ()
   printquine = getArgs >>= (putStrLn . unwords)
   
   -- Increments the accumulator. You can insert print statements to verify correctness.
-  increment :: IORef Integer -> IO ()
-  increment ref = modifyIORef ref (\x -> x + 1)
+  increment :: Char -> Integer -> Integer
+  increment '+' accum = accum + 1
+  increment _ accum = accum
   
   -- The actual parsing function.
-  parse :: IORef Integer -> Char -> IO ()
-  parse ref c =
-    case c of
-      'h' -> putStrLn "Hello, world."
-      'q' -> printquine
-      '9' -> bottles 9
-      '+' -> increment ref
-      otherwise -> return ()
+  parse :: Integer -> String -> IO ()
+  parse _ "" = return ()
+  parse accum string = do
+    let c = (string !! 0) in do
+      case c of
+        'h' -> putStrLn "Hello, world."
+        'q' -> printquine
+        '9' -> bottles 9
+        '+' -> return ()
+      -- print accum -- Uncomment this to prove that the accumulator working.
+      parse (increment c accum) (drop 1 string)
   
   main :: IO ()
   main = do
-    ref <- newIORef 0 -- initialize the accumulator to 0
     args <- getArgs
-    -- monadically map the parse command onto the concatenated arguments
-    mapM_ (parse ref) (unwords args)
+    parse 1 (unwords args)
+  -- Alternatively, you can do this (more concise but less readable)
+  -- main = getArgs >>= (parse 1 . unwords)
